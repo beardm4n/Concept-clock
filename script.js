ebecbef6e887ec9925482d626b77b1ba15386f3b
beardm4n <dmitriykorovko@gmail.com>
2019-08-31 11:09:55 +0300

    created a clock

---
+++ script.js
@@ -0,0 +1,23 @@
+let clock = document.querySelector('#clock'),
+    color = document.querySelector('#color');
+
+function getTime() {
+   let time = new Date(),
+       hour = (time.getHours() % 12).toString(),
+       min = time.getMinutes().toString(),
+       sec = time.getSeconds().toString();
+
+   if (hour.length < 2) hour = '0' + hour;
+   if (min.length < 2) min = '0' + min;
+   if (sec.length < 2) sec = '0' + sec;
+
+   let clockString = hour + ':' + min + ':' + sec;
+   let colorString = '#' + hour + min + sec;
+
+   clock.textContent = clockString;
+   color.textContent = colorString;
+
+   document.body.style.background = colorString;
+}
+getTime();
+setInterval(getTime, 1000);
