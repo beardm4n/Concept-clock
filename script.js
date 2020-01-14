fc84b2d47175028f6231bdb2c93755a8f08df86c
beardm4n <dmitriykorovko@gmail.com>
2020-01-15 01:30:22 +0300

    clock created

--- script.js
+++ script.js
@@ -1,23 +1,66 @@
-let clock = document.querySelector('#clock'),
-    color = document.querySelector('#color');
+const day = document.querySelector('.day');
+const hour = document.querySelector('.progress-ring__hours');
+const min = document.querySelector('.progress-ring__minutes');
+const sec = document.querySelector('.progress-ring__seconds');
+const radiusH = hour.r.baseVal.value;
+const radiusM = min.r.baseVal.value;
+const radiusS = sec.r.baseVal.value;
+// calculate the circumference
+const circumferenceH = 2 * Math.PI * radiusH;
+const circumferenceM = 2 * Math.PI * radiusM;
+const circumference = 2 * Math.PI * radiusS;
 
-function getTime() {
-   let time = new Date(),
-       hour = time.getHours().toString(),
-       min = time.getMinutes().toString(),
-       sec = time.getSeconds().toString();
+hour.style.strokeDasharray = `${circumferenceH} ${circumferenceH}`;
+hour.style.strokeDashoffset = circumferenceH;
 
-   if (hour.length < 2) hour = '0' + hour;
-   if (min.length < 2) min = '0' + min;
-   if (sec.length < 2) sec = '0' + sec;
+min.style.strokeDasharray = `${circumferenceM} ${circumferenceM}`;
+min.style.strokeDashoffset = circumferenceM;
 
-   let clockString = hour + ':' + min + ':' + sec;
-   let colorString = '#' + hour + min + sec;
+sec.style.strokeDasharray = `${circumference} ${circumference}`;
+sec.style.strokeDashoffset = circumference;
 
-   clock.textContent = clockString;
-   color.textContent = colorString;
-
-   document.body.style.background = colorString;
+function setHours(hours) {
+    const offset = circumferenceH - hours / 12 * circumferenceH;
+    hour.style.strokeDashoffset = offset;
 }
-getTime();
-setInterval(getTime, 1000);
+
+function setMinutes(minutes) {
+    const offset = circumferenceM - minutes / 60 * circumferenceM;
+    min.style.strokeDashoffset = offset;
+}
+
+function setSeconds(seconds) {
+    const offset = circumference - seconds / 60 * circumference;
+    sec.style.strokeDashoffset = offset;
+}
+
+function getHours() {
+    const time = new Date(),
+          h = time.getHours();
+
+    (h >= 0 && h < 12) ? day.textContent = 'AM' : day.textContent = 'PM'; 
+
+    if (h >= 0 && h < 12) {
+        setHours(h);
+    } else {
+        setHours(h - 12);
+    }
+}
+
+setInterval(getHours, 1000)
+
+function getMinutes() {
+    const time = new Date(),
+          m = time.getMinutes() + 1;
+    setMinutes(m);
+}
+
+setInterval(getMinutes, 1000)
+
+function getSeconds() {
+    const time = new Date();
+    let s = time.getSeconds() + 1;
+    setSeconds(s);
+}
+
+setInterval(getSeconds, 500);
